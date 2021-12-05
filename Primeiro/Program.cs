55acd5ac2bf997df2c15c16f3a214d29fd561d8f
samuelikz <samuel.nds@outlook.com>
2021-12-05 13:42:33 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello";
+            string b = "World!";
+
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
